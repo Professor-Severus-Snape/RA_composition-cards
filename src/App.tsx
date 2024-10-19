36f9c6aa86187fcd42da4c986e364f2897325bcb
Julia Fokanova <oncologmma@mail.ru>
2024-10-19 18:25:56 +0300

    task done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import Card from './components/Card/Card';
+import data from './data/data.json'; // данные карточек в json
+import cardImage from './assets/card-image.png'; // картинка
+
 const App = () => {
-  return <>hello</>;
+  return (
+    <>
+      {/* NOTE: Вариант без chidren: */}
+      {data.map((item) => (
+        <Card key={item.id} item={item} />
+      ))}
+
+      {/* NOTE: Вариант с chidren: */}
+      <Card item={{...data[0], title: "Card title 8", text: "This is a card with children!!!"}}>
+        <img className="card__img" src={cardImage} alt="card not found" />
+      </Card>
+    </>
+  );
 };
 
 export default App;
